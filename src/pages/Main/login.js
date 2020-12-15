9b74072bad66f61aa0440e98ebb48c3a559b627e
Ítalo Rennan <italorennanls@gmail.com>
2020-12-15 15:41:09 -0300

    front

--- src/pages/Main/login.js
+++ src/pages/Main/login.js
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 import { Container } from './styles';
+import api from '../../services/api';
 
 function Login() {
     // Hook de estado para variáveis de login
     const [state, setState] = useState({
-        user: '',
+        username: '',
         password: ''
     });
 
-    function handleChangePassword(password){
-        
-    }
+    // Função para enviar dados de login ao back
+    async function enter() {
+        const data = {
+            username: state.username,
+            password: state.password
+        };
 
-    function handleChangeUser(user){
-
-    }
-
-    function handleLoginSubmit(){
-
+        // Chamar rota para envio dos dados
+        await api.post('/login',
+            JSON.stringify(data), { headers: {'Content-Type': 'application/json'} }
+        );
     }
 
     return (
@@ -26,10 +28,12 @@ function Login() {
                 <h1>Login</h1>
 
                 <h2>Usuário:</h2>
-                <input type="text" name="user"onChange={e=> handleChangeUser(e.target.value)} required/>
+                <input type="text" name="username" onChange={e => setState({...state, username: e.target.value.replace('<', '\u003c').replace('>', '\u003e')})}/>
+                <br /><br />
                 <h2>Senha:</h2>
-                <input type="password" name="password" onChange={e=>handleChangePassword(e.target.value)} required/>
-                <input type="submit" value="Submit" onClick={e=>{e.preventDefault(); handleLoginSubmit();}}required/>
+                <input type="password" name="password" onChange={e => setState({...state, password: e.target.value.replace('<', '\u003c').replace('>', '\u003e')})} required/>
+                <br /><br />
+                <button onClick={() => enter()}>Entrar</button>
             </Container>
         </>
     );
