5be4a8e6ccf68ff6e620cd46ed40eb291daf6894
Ítalo Rennan <italorennanls@gmail.com>
2020-12-15 15:28:20 -0300

    front10

--- src/pages/Main/login.js
+++ src/pages/Main/login.js
@@ -1,25 +1,39 @@
 import React, { useState } from 'react';
 import { Container } from './styles';
+import api from '../../services/api';
 
 function Login() {
     // Hook de estado para variáveis de login
     const [state, setState] = useState({
-        user: '',
+        username: '',
         password: ''
     });
+
+    // Função para enviar dados de login ao back
+    async function enter() {
+        const data = {
+            username: state.username,
+            password: state.password
+        };
+
+        // Chamar rota para envio dos dados
+        await api.post('/login',
+            JSON.stringify(data), { headers: {'Content-Type': 'application/json'} }
+        );
+    }
 
     return (
         <>
             <Container>
                 <h1>Login</h1>
 
-                <form action="https://stormy-garden-64077.herokuapp.com/login" method="post">
-                    <h2>Usuário:</h2>
-                    <input type="text" name="username"/>
-                    <h2>Senha:</h2>
-                    <input type="password" name="password" required/>
-                    <input class="submit-btn" type="submit" value="Submit" required/>
-                </form>
+                <h2>Usuário:</h2>
+                <input type="text" name="username" onChange={e => setState({...state, username: e.target.value.replace('<', '\u003c').replace('>', '\u003e')})}/>
+                <br /><br />
+                <h2>Senha:</h2>
+                <input type="password" name="password" onChange={e => setState({...state, password: e.target.value.replace('<', '\u003c').replace('>', '\u003e')})} required/>
+                <br /><br />
+                <button onClick={() => enter()}>Entrar</button>
             </Container>
         </>
     );
